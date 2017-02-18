b4c7432ee76fe4853374a3571141153d71a02ffb
vsheremet92 <vsheremet92@gmail.com>
2017-02-18 21:25:42 +0200

    Severely enhanced functionality - now calc supports enter from keyboard. You can perform any actions.

--- core/index.js
+++ core/index.js
@@ -12,46 +12,72 @@ var getResult = $('#get-result');
 var currNumber = '';
 var firstNumber = '';
 var secondNumber = '';
+
 var currentAction = '';
 
 var result = 0;
 
-function digitPressed(e) {
+function digitPressed(e, diff) {
     if (currNumber === '0' || result === '0') currNumber = '';
-    currNumber+=$(this).html();
-    console.log(e);
-    //else if (48 < digit < 57) currNumber += String(digit-48);
-    //else return;
+    switch (e.type) {
+        case 'click':
+            currNumber+=$(this).html();
+            break;
+        case 'keydown':
+            currNumber+=e.which - diff;            
+            break;
+    }
     state.val(currNumber);
 }
 
-/* $(document).keydown(function (e) {
-    digitPressed();
-}); */
+function clearState () {
+    state.val(0);
+    currNumber = '0';
+}
 
-digit.click(digitPressed);
-
-action.click (function () {
+function completeAction(e) {
     firstNumber = currNumber;
     state.val(firstNumber);
     currNumber = '0';
-    switch($(this).attr('id')) {
-        case 'div':
-            currentAction = 'divide';
+    switch(e.type) {
+        case 'click':
+            switch($(this).attr('id')) {
+            case 'div':
+                currentAction = 'divide';
+                break;
+            case 'mult':
+                currentAction = 'multiply';
+                break;
+            case 'sub':
+                currentAction = 'subtract';
+                break;
+            case 'add':
+                currentAction = 'addition';
+                break;
+            }
             break;
-        case 'mult':
-            currentAction = 'multiply';
-            break;
-        case 'sub':
-            currentAction = 'subtract';
-            break;
-        case 'add':
-            currentAction = 'addition';
-            break;
+        case 'keydown':  
+            switch(e.which) {
+            case 111:
+            case 191:
+                currentAction = 'divide';
+                break;
+            case 106:
+                currentAction = 'multiply';
+                break;
+            case 109:
+            case 189:
+                currentAction = 'subtract';
+                break;
+            case 107:
+                currentAction = 'addition';
+                break;
+            }
+            break;   
     };
-});
+}
 
-getResult.click (function () {
+function equals() {
     secondNumber = currNumber;
     switch (currentAction) {
         case 'divide':
@@ -67,17 +93,37 @@ getResult.click (function () {
             result = Number(firstNumber) + Number(secondNumber);
             break;
     }
-    state.val(result);
     currNumber = result;
+    state.val(currNumber);
     currentAction = '';
+}
+
+function deleteSymbol() {
+    currNumber = currNumber.slice(0, currNumber.length-1);
+    state.val(currNumber);
+}
+
+$(document).keydown(function(e) {
+    var arrOfKeyActions = [106, 107, 109, 111, 189, 191];
+    if (e.which >= 48 && e.which <= 57) digitPressed(e, 48);
+    if (e.which >= 96 && e.which <= 105) digitPressed(e, 96);
+    for (var i=0; i<arrOfKeyActions.length; i++) {
+        if (e.which === arrOfKeyActions[i]) completeAction(e);    
+    } 
+    if (e.which === 27) clearState();
+    if (e.which === 13 || e.which === 187) equals();
+    if (e.which === 8) deleteSymbol();
 });
+
+digit.click(digitPressed);
+
+action.click(completeAction);
+
+getResult.click(equals);
 
 plusMinus.click(function () {
     currNumber === 0 ? currNumber='0' : currNumber=-currNumber;
     state.val(currNumber);
 })
 
-clear.click(function () {
-    state.val(0);
-    currNumber = '0';
-});
+clear.click(clearState);
